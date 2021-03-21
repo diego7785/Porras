041d5e5c4815f74af37cd7201346b23ab3f61aec
diego7785 <diego.viafara@correounivalle.edu.co>
2021-03-21 14:01:22 -0500

    adding database code

---
+++ bdd.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Person(
+    id VARCHAR(10), 
+    name VARCHAR(255), 
+    email VARCHAR(255), 
+    password VARCHAR(255), 
+    CONSTRAINT Pk_user PRIMARY KEY(id));
+
+CREATE TABLE Token (
+    token VARCHAR(10), 
+    id_person VARCHAR(10), 
+    CONSTRAINT pk_token PRIMARY KEY (token), 
+    CONSTRAINT fk_person_token FOREIGN KEY (id_person) REFERENCES Person(id) )
