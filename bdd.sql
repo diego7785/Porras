0b8b2ff0cb687b01cbdfc882cfcf7b235f048fa0
Diego Andrés Bonilla Viáfara <50996940+diego7785@users.noreply.github.com>
2021-03-21 23:51:44 -0500

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -6,7 +6,7 @@ CREATE TABLE Person(
     CONSTRAINT Pk_user PRIMARY KEY(id));
 
 CREATE TABLE Token (
-    token VARCHAR(10), 
+    token VARCHAR(255), 
     id_person VARCHAR(10), 
     CONSTRAINT pk_token PRIMARY KEY (token), 
     CONSTRAINT fk_person_token FOREIGN KEY (id_person) REFERENCES Person(id) )
